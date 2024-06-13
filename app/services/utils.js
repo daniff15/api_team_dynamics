b75c90cca5edf4814a1fda9e5a2495d4a739fe75
Dani Figueiredo <dani.fig@ua.pt>
2024-06-13 22:33:10 +0100

    Fix: Forget the update element relationship (Future work)

--- app/services/utils.js
+++ app/services/utils.js
@@ -53,72 +53,6 @@ const getAllElements = async () => {
     }
 };
 
-
-// const updateElementRelationships = async (elementId, strengths, weaknesses) => {
-//     try {
-//         // Fetch the element to ensure it exists
-//         const element = await ElementsModel.findByPk(elementId);
-//         if (!element) {
-//             return NotFoundError('Element not found');
-//         }
-
-//         const strengthId = strengths[0];
-//         const weaknessId = weaknesses[0];
-
-//         const strengthIdExist = await ElementsModel.findByPk(strengthId);
-//         const weaknessIdExist = await ElementsModel.findByPk(weaknessId);
-
-//         if (!strengthIdExist) {
-//             return NotFoundError('Strength element not found');
-//         }
-
-//         if (!weaknessIdExist) {
-//             return NotFoundError('Weakness element not found');
-//         }
-
-//         // Add new strengths
-//         if (Array.isArray(strengths)) {
-//             if (strengths.length > 1) {
-//                 return BadRequestError('An element can have only one strength');
-//             }
-//         }
-
-//         if (Array.isArray(weaknesses)) {
-//             if (weaknesses.length > 1) {
-//                 return BadRequestError('An element can have only one weakness');
-//             }
-//         }
-
-//         // Remove existing relationships
-//         await ElementRelationshipsModel.destroy({ where: { element_id: elementId } });
-
-//         // Create new relationships
-//         await ElementRelationshipsModel.create({
-//             element_id: elementId,
-//             strong_against_id: strengthId,
-//             weak_against_id: weaknessId,
-//         });
-
-//         const response = {
-//             id: parseInt(elementId),
-//             name: element.name,
-//             strengths: {
-//                 id: strengthId,
-//                 name: strengthId ? (await ElementsModel.findByPk(strengthId)).name : null,
-//             },
-//             weaknesses: {
-//                 id: weaknessId,
-//                 name: weaknessId ? (await ElementsModel.findByPk(weaknessId)).name : null,
-//             },
-//         };
-
-//         return success(response);
-//     } catch (error) {
-//         return ServerError(error.message);
-//     }
-// };
-
-
 const getAllAttributes = async () => {
     const attributes = await AttributesModel.findAll({});
     return success(attributes);
@@ -152,7 +86,6 @@ const updateMaxLevel = async (level) => {
 
 module.exports = {
     getAllElements,
-    // updateElementRelationships,
     getAllAttributes,
     getMaxLevel,
     updateMaxLevel,
