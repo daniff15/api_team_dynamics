b2c456144905f62c1f11b700cb84c1aec57df29a
Dani Figueiredo <dani.fig@ua.pt>
2024-06-17 10:53:07 +0100

    Fix: docker running on different pcs

--- docker/mysql/initdb.d/00-create-database.sql
+++ docker/mysql/initdb.d/00-create-database.sql
@@ -1,4 +1,6 @@
 CREATE DATABASE IF NOT EXISTS team_dynamics;
+GRANT ALL PRIVILEGES ON team_dynamics.* TO 'root'@'%';
+FLUSH PRIVILEGES;
 SET GLOBAL sql_mode = REPLACE(@@sql_mode, 'ONLY_FULL_GROUP_BY', '');
 SET SESSION group_concat_max_len = 10000; 
 SET GLOBAL time_zone = '+00:00';
